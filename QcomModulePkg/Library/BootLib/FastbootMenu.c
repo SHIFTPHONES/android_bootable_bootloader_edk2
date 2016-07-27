463e3cb21296547fc6af374c5b349378aa9a28e7
lijuang <lijuang@codeaurora.org>
2016-07-27 19:26:38 +0800

    QcomModulePkg: Add support for fastboot menu
    Add functions to draw fastboot menu
    
    CRs-Fixed: 1042864
    Change-Id: I18dc1352c0b0f82b259cb16faa461055bb76e3ce

---
+++ QcomModulePkg/Library/BootLib/FastbootMenu.c
@@ -0,0 +1,252 @@
+/* Copyright (c) 2016, The Linux Foundation. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * * Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials provided
+ *  with the distribution.
+ *   * Neither the name of The Linux Foundation nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+ * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <Uefi.h>
+#include <Library/BaseLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/DebugLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/UefiHiiServicesLib.h>
+#include <Library/DeviceInfo.h>
+#include <Library/DrawUI.h>
+#include <Library/FastbootMenu.h>
+#include <Library/MenuKeysDetection.h>
+#include <Library/UpdateDeviceTree.h>
+#include <Protocol/EFIVerifiedBoot.h>
+
+STATIC OPTION_MENU_INFO gMenuInfo;
+
+STATIC MENU_MSG_INFO mFastbootOptionTitle[] = {
+	{{"START"},
+		BIG_FACTOR, BGR_GREEN, BGR_BLACK, OPTION_ITEM, 0, RESTART},
+	{{"Restart bootloader"},
+		BIG_FACTOR, BGR_RED, BGR_BLACK, OPTION_ITEM, 0, FASTBOOT},
+	{{"Recovery mode"},
+		BIG_FACTOR, BGR_RED, BGR_BLACK, OPTION_ITEM, 0, RECOVER},
+	{{"Power off"},
+		BIG_FACTOR, BGR_WHITE, BGR_BLACK, OPTION_ITEM, 0, POWEROFF},
+	{{"Boot to FFBM"},
+		BIG_FACTOR, BGR_YELLOW, BGR_BLACK, OPTION_ITEM, 0, FFBM},
+};
+
+STATIC MENU_MSG_INFO mFastbootCommonMsgInfo[] = {
+	{{"\nPress volume key to select, "\
+			"and press power key to select\n\n"},
+		COMMON_FACTOR, BGR_WHITE, BGR_BLACK, COMMON, 0, NOACTION},
+	{{"FastBoot Mode"},
+		COMMON_FACTOR, BGR_RED, BGR_BLACK, COMMON, 0, NOACTION},
+	{{"PRODUCT_NAME - "},
+		COMMON_FACTOR, BGR_WHITE, BGR_BLACK, COMMON, 0, NOACTION},
+	{{"VARIANT - "},
+		COMMON_FACTOR, BGR_WHITE, BGR_BLACK, COMMON, 0, NOACTION},
+	{{"BOOTLOADER VERSION - "},
+		COMMON_FACTOR, BGR_WHITE, BGR_BLACK, COMMON, 0, NOACTION},
+	{{"BASEBAND VERSION - "},
+		COMMON_FACTOR, BGR_WHITE, BGR_BLACK, COMMON, 0, NOACTION},
+	{{"SERIAL NUMBER - "},
+		COMMON_FACTOR, BGR_WHITE, BGR_BLACK, COMMON, 0, NOACTION},
+	{{"SECURE BOOT - "},
+		COMMON_FACTOR, BGR_WHITE, BGR_BLACK, COMMON, 0, NOACTION},
+	{{"DEVICE STATE - "},
+		COMMON_FACTOR, BGR_RED, BGR_BLACK, COMMON, 0, NOACTION},
+};
+
+/**
+  Update the fastboot option item
+  @param[in] OptionItem  The new fastboot option item
+  @param[out] pLocation  The pointer of the location
+  @retval EFI_SUCCESS	 The entry point is executed successfully.
+  @retval other		 Some error occurs when executing this entry point.
+ **/
+EFI_STATUS UpdateFastbootOptionItem(UINT32 OptionItem, UINT32 *pLocation)
+{
+	UINT32  Location = 0;
+	UINT32  Height = 0;
+	MENU_MSG_INFO *FastbootLineInfo = NULL;
+
+	FastbootLineInfo = AllocateZeroPool(sizeof(MENU_MSG_INFO));
+	if (FastbootLineInfo == NULL) {
+		DEBUG((EFI_D_ERROR, "Failed to allocate zero pool.\n"));
+		return EFI_OUT_OF_RESOURCES;
+	}
+
+	SetMenuMsgInfo(FastbootLineInfo, "__________",
+		COMMON_FACTOR,
+		mFastbootOptionTitle[OptionItem].FgColor,
+		mFastbootOptionTitle[OptionItem].BgColor,
+		LINEATION,
+		Location,
+		NOACTION);
+	DrawMenu(FastbootLineInfo, &Height);
+	Location += Height;
+
+	mFastbootOptionTitle[OptionItem].Location = Location;
+	DrawMenu(mFastbootOptionTitle[OptionItem].Msg, &Height);
+	Location += Height;
+
+	FastbootLineInfo->Location = Location;
+	DrawMenu(FastbootLineInfo, &Height);
+	Location += Height;
+
+	if (FastbootLineInfo) {
+		FreePool(FastbootLineInfo);
+		FastbootLineInfo = NULL;
+	}
+
+	if (pLocation != NULL)
+		*pLocation = Location;
+
+	return EFI_SUCCESS;
+}
+
+/**
+  Draw the fastboot menu
+  @param[out] OptionMenuInfo  Fastboot option info
+  @retval     EFI_SUCCESS     The entry point is executed successfully.
+  @retval     other           Some error occurs when executing this entry point.
+ **/
+STATIC EFI_STATUS FastbootMenuShowScreen(OPTION_MENU_INFO *OptionMenuInfo)
+{
+	EFI_STATUS Status;
+	UINT32  Location = 0;
+	UINT32  OptionItem = 0;
+	UINT32  Height = 0;
+	UINT32  i = 0;
+	CHAR8 SerialNum[MAX_RSP_SIZE];
+	DeviceInfo *DevInfo = NULL;
+
+	/* Clear the screen before launch the fastboot menu */
+	gST->ConOut->ClearScreen (gST->ConOut);
+	ZeroMem(&OptionMenuInfo->Info, sizeof(MENU_OPTION_ITEM_INFO));
+
+	/* Read Device Info */
+	DevInfo = AllocateZeroPool(sizeof(DeviceInfo));
+	if (DevInfo == NULL) {
+		DEBUG((EFI_D_ERROR, "Failed to allocate zero pool for device info.\n"));
+		return EFI_OUT_OF_RESOURCES;
+	}
+
+	Status = ReadWriteDeviceInfo(READ_CONFIG, (UINT8 *)DevInfo, sizeof(DeviceInfo));
+	if (Status != EFI_SUCCESS)
+	{
+		/* Show empty value on the screen instead of device info */
+		DEBUG((EFI_D_ERROR, "Unable to Read Device Info: %r\n", Status));
+	}
+
+	/* Update fastboot option title */
+	OptionMenuInfo->Info.MsgInfo = mFastbootOptionTitle;
+	for (i = 0; i < ARRAY_SIZE(mFastbootOptionTitle); i++) {
+		OptionMenuInfo->Info.OptionItems[i] = i;
+	}
+	OptionItem = OptionMenuInfo->Info.OptionItems[OptionMenuInfo->Info.OptionIndex];
+	UpdateFastbootOptionItem(OptionItem, &Location);
+
+	/* Update fastboot common message */
+	for (i = 0; i < ARRAY_SIZE(mFastbootCommonMsgInfo); i++) {
+		switch (i) {
+		case 0:
+		case 1:
+			break;
+		case 2:
+			/* Get product name */
+			AsciiStrnCat(mFastbootCommonMsgInfo[i].Msg,
+				"unsupported", MAX_RSP_SIZE);
+			break;
+		case 3:
+			/* Get variant value */
+			AsciiStrnCat(mFastbootCommonMsgInfo[i].Msg,
+				"unsupported", MAX_RSP_SIZE);
+			break;
+		case 4:
+			/* Get bootloader version */
+			AsciiStrnCat(mFastbootCommonMsgInfo[i].Msg,
+				DevInfo->bootloader_version, MAX_RSP_SIZE);
+			break;
+		case 5:
+			/* Get baseband version */
+			AsciiStrnCat(mFastbootCommonMsgInfo[i].Msg,
+				DevInfo->radio_version, MAX_RSP_SIZE);
+			break;
+		case 6:
+			/* Get serial number */
+			ZeroMem(SerialNum, sizeof(SerialNum));
+			BoardSerialNum(SerialNum, MAX_RSP_SIZE);
+			AsciiStrnCat(mFastbootCommonMsgInfo[i].Msg,
+				SerialNum, MAX_RSP_SIZE);
+			break;
+		case 7:
+			/* Get secure boot value */
+			AsciiStrnCat(mFastbootCommonMsgInfo[i].Msg,
+				"unsupported", MAX_RSP_SIZE);
+			break;
+		case 8:
+			/* Get device status */
+			AsciiStrnCat(mFastbootCommonMsgInfo[i].Msg,
+				DevInfo->is_unlocked ? "locked":"unlocked", MAX_RSP_SIZE);
+			break;
+		}
+
+		mFastbootCommonMsgInfo[i].Location = Location;
+		DrawMenu(&mFastbootCommonMsgInfo[i], &Height);
+		Location += Height;
+	}
+
+	OptionMenuInfo->Info.MenuType = DISPLAY_MENU_FASTBOOT;
+	OptionMenuInfo->Info.OptionNum = ARRAY_SIZE(mFastbootOptionTitle);
+
+	if (DevInfo) {
+		FreePool(DevInfo);
+		DevInfo = NULL;
+	}
+
+	return EFI_SUCCESS;
+}
+
+/* Draw the fastboot menu and start to detect the key's status */
+VOID DisplayFastbootMenu()
+{
+	EFI_STATUS Status;
+	OPTION_MENU_INFO *OptionMenuInfo;
+	OptionMenuInfo = &gMenuInfo;
+
+	if (FixedPcdGetBool(EnableDisplayMenu)) {
+		OptionMenuInfo->LastMenuType =
+			OptionMenuInfo->Info.MenuType;
+
+		Status = FastbootMenuShowScreen(OptionMenuInfo);
+		if (Status != EFI_SUCCESS) {
+			DEBUG((EFI_D_ERROR, "Unable to show fastboot menu on screen: %r\n", Status));
+			return;
+		}
+
+		MenuKeysDetectionInit(OptionMenuInfo);
+		DEBUG((EFI_D_INFO, "Creating fastboot menu keys detect event\n"));
+	} else {
+		DEBUG((EFI_D_INFO, "Display menu is not enabled!\n"));
+	}
+}
