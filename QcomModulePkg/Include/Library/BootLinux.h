e9f0a9714c36e894681ea63612bd6c536a7496fa
Jeevan Shriram <jshriram@codeaurora.org>
2016-09-22 18:50:30 -0700

    QcomModulePkg: Refactor Update Command Line code
    Refactor Update Command Line code base to follow UEFI coding guidelines
    and update proper return status to the caller.
    
    CRs-Fixed: 1070428
    Change-Id: Ie03c69dadf836defac20bf6ffa09402b00b651e0

--- QcomModulePkg/Include/Library/BootLinux.h
+++ QcomModulePkg/Include/Library/BootLinux.h
@@ -83,4 +83,5 @@ EFI_STATUS BootLinux(VOID *ImageBuffer, UINT32 ImageSize, DeviceInfo *DevInfo, C
 EFI_STATUS CheckImageHeader (VOID *ImageHdrBuffer, UINT32 ImageHdrSize, UINT32 *ImageSizeActual, UINT32 *PageSize);
 EFI_STATUS LoadImage (CHAR8 *Pname, VOID **ImageBuffer, UINT32 *ImageSizeActual);
 EFI_STATUS LaunchApp(IN UINTN  Argc, IN CHAR8  **Argv);
+BOOLEAN VerifiedBootEnbled();
 #endif
