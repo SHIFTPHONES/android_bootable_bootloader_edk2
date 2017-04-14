43aa02fa1ea2a79d8d10f83e35de10ee626e7cb9
lijuang <lijuang@codeaurora.org>
2017-04-14 10:39:10 +0800

    QcomModulePkg: Add new charger protocol functions for battery and power off mode checking
    Add new charger protocol functions
    1. EFI_CHARGER_EX_IS_OFFMODE_CHARGING: power off charging mode's condition checking
    2. EFI_CHARGER_EX_IS_POWER_OK: battery safeguards checking
    
    Change-Id: I456b7052b0c2f8dce1d893cb83dbb721b9b0e126

--- QcomModulePkg/Include/Protocol/EFIChargerEx.h
+++ QcomModulePkg/Include/Protocol/EFIChargerEx.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2016-2017, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -37,7 +37,10 @@ EDIT HISTORY
 
 
 when       who     what, where, why
---------   ---     ----------------------------------------------------------- 
+--------   ---     -----------------------------------------------------------
+03/03/17   va      Adding offmode, flash APIs to contained for ABL, making open
+                   source and local header copies same since ABL need those and
+                   internally no calls being made
 05/04/16   sm      New file.
 
 =============================================================================*/
@@ -51,7 +54,7 @@ MACRO DECLARATIONS
 Protocol version.
 */
 
-#define CHARGER_EX_REVISION 0x0000000000010002
+#define CHARGER_EX_REVISION 0x0000000000010003
 /** @} */ /* end_addtogroup efi_ChargerEx_constants */
 
 /*  Protocol GUID definition */
@@ -66,18 +69,30 @@ EXTERNAL VARIABLES
 /**
 External reference to the Charger Extended Protocol GUID.
 */
-extern EFI_GUID gQcomChargerExProtocolGuid;
+extern EFI_GUID gChargerExProtocolGuid;
 
 /*===========================================================================
 TYPE DEFINITIONS
 ===========================================================================*/
+typedef enum {
+  EFI_CHARGER_EX_POWER_FLASH_BATTERY_VOLTAGE_TYPE,
+  EFI_CHARGER_EX_POWER_TYPE_INVALID = 0x7FFFFFFF
+}EFI_CHARGER_EX_POWER_TYPE;
+
+
+typedef struct {
+  BOOLEAN bCanFlash;
+  UINT32  BattCurrVoltage;
+  UINT32  BattRequiredVoltage;
+}EFI_CHARGER_EX_FLASH_INFO;
+
 /**
 Protocol declaration.
 */
-typedef struct _EFI_QCOM_CHARGER_EX_PROTOCOL   EFI_QCOM_CHARGER_EX_PROTOCOL;
+typedef struct _EFI_CHARGER_EX_PROTOCOL   EFI_CHARGER_EX_PROTOCOL;
 /** @endcond */
 
-/** @addtogroup efi_ChargerEx_data_types 
+/** @addtogroup efi_ChargerEx_data_types
 @{ */
 
 
@@ -137,6 +152,42 @@ EFI_STATUS (EFIAPI *EFI_CHARGER_EX_GET_BATTERY_PRESENCE)
   OUT BOOLEAN  *pBatteryPresent
 );
 
+/* EFI_CHARGER_EX_IS_OFFMODE_CHARGING */
+/**@ingroup efi_charger_ex_is_offmode_charging
+  @par Summary
+  This API returns if device needs to off mode charging
+
+  @param[out]  bOffModeCharging   TRUE:OffMode Charging is needed, Otherwise FALSE
+
+  @return
+  EFI_SUCCESS            -- Function completed successfully.
+  EFI_DEVICE_ERROR       -- Physical device reported an error.
+*/
+typedef
+EFI_STATUS ( EFIAPI * EFI_CHARGER_EX_IS_OFFMODE_CHARGING)
+(
+  OUT BOOLEAN *bOffModeCharging
+);
+
+
+/* EFI_CHARGER_EX_IS_POWER_OK */
+/**@ingroup efi_charger_ex_is_power_ok
+  @par Summary
+  This API returns if device battery voltage is good for SW image flash
+
+  @param[in]   FlashType     Refer EFI_CHARGER_EX_POWER_TYPE
+  @param[out]  PowerTypeInfo Refer required queried structure
+
+  @return
+  EFI_SUCCESS            -- Function completed successfully.
+  EFI_DEVICE_ERROR       -- Physical device reported an error.
+*/
+typedef
+EFI_STATUS ( EFIAPI * EFI_CHARGER_EX_IS_POWER_OK)
+(
+  IN  EFI_CHARGER_EX_POWER_TYPE PowerType,
+  OUT VOID                     *PowerTypeInfo
+);
 
 /*===========================================================================
 PROTOCOL INTERFACE
@@ -149,11 +200,13 @@ PMIC Charger Protocol interface.
 @inputprotoparams{pmic_charger_proto_params.tex} 
 */
 
-struct _EFI_QCOM_CHARGER_EX_PROTOCOL {
+struct _EFI_CHARGER_EX_PROTOCOL {
   UINT64                                Revision;
   EFI_CHARGER_EX_GET_CHARGER_PRESENCE   GetChargerPresence;    // Returns if charger is present
   EFI_CHARGER_EX_GET_BATTERY_PRESENCE   GetBatteryPresence;    // Returns if battery is present
   EFI_CHARGER_EX_GET_BATTERY_VOLTAGE    GetBatteryVoltage;     // Returns battery voltage
+  EFI_CHARGER_EX_IS_OFFMODE_CHARGING    IsOffModeCharging;
+  EFI_CHARGER_EX_IS_POWER_OK            IsPowerOk;
 };
 
 
