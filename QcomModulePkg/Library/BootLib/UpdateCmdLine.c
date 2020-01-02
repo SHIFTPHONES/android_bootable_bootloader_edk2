7943a53a10f53a164f8b3d43ae9922f85e214c9d
Mayank Grover <groverm@codeaurora.org>
2020-01-02 13:47:53 +0530

    QcomModulePkg: append force_normal_boot with dp and recovery as boot enabled
    For devices where dyanamic partition(DP) is not enabled and system as root
    and recovery as boot both flags are enabled, force_normal_boot=1 flag is
    not required to boot into normal mode or recovery mode.
    For normal bootup system is mounted at / in RO mode hence mkdir and
    switch root to /first_stage_ramdisk will fail if force_normal_boot is
    passed in commandline.
    
    Change-Id: I79e9ebc18d0b020a3e7280098889a8e1ddf6fc56

--- QcomModulePkg/Library/BootLib/UpdateCmdLine.c
+++ QcomModulePkg/Library/BootLib/UpdateCmdLine.c
@@ -3,7 +3,7 @@
  * Copyright (c) 2009, Google Inc.
  * All rights reserved.
  *
- * Copyright (c) 2009-2019, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2009-2020, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -509,6 +509,7 @@ UpdateCmdLineParams (UpdateCmdLineParamList *Param,
   }
 
   if (IsBuildUseRecoveryAsBoot () &&
+      IsDynamicPartitionSupport () &&
       !Param->Recovery) {
     Src = AndroidBootForceNormalBoot;
     AsciiStrCatS (Dst, MaxCmdLineLen, Src);
@@ -692,6 +693,7 @@ UpdateCmdLine (CONST CHAR8 *CmdLine,
   }
 
   if (IsBuildUseRecoveryAsBoot () &&
+      IsDynamicPartitionSupport () &&
       !Recovery) {
     CmdLineLen += AsciiStrLen (AndroidBootForceNormalBoot);
   }
