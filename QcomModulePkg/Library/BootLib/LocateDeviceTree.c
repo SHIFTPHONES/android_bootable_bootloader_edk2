8ba3fdde6ecc3c3f9f35137b544d24b5a4bc9996
Lijuan Gao <lijuang@codeaurora.org>
2020-09-21 20:29:27 +0800

    QcomModulePkg: Fixed the possible security issue when getting partition size
    Fixed the possible security issue when getting partition size.
    1. Interger overflow while adding LastBlock and 1.
    2. Integer overflow while multiplying LastBlock and BlockSize.
    
    Change-Id: Idd103ba31cffcf407520c5c65a9cb46d71ca2dfa

--- QcomModulePkg/Library/BootLib/LocateDeviceTree.c
+++ QcomModulePkg/Library/BootLib/LocateDeviceTree.c
@@ -1035,7 +1035,11 @@ GetOvrdDtb ( VOID **DtboImgBuffer)
   }
 
   BlockIo = HandleInfoList[0].BlkIo;
-  DtboImgSz = (BlockIo->Media->LastBlock + 1) * BlockIo->Media->BlockSize;
+  DtboImgSz = GetPartitionSize (BlockIo);
+  if (!DtboImgSz) {
+    Status = EFI_BAD_BUFFER_SIZE;
+    goto err;
+  }
   *DtboImgBuffer = AllocateZeroPool (DtboImgSz);
   if (*DtboImgBuffer == NULL) {
     DEBUG ((EFI_D_ERROR, "Override DTB: Buffer allocation failure\n"));
