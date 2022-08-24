8aaf4ef261efd153beada54806098375a4fe2f2a
Guru Das Srinagesh <quic_gurus@quicinc.com>
2022-08-23 17:32:13 -0700

    edk2: QcomModulePkg: Remove waits in selected error paths
    There is no reason for these waits to be present in these error paths as
    the waits are to be carried out in the good case, i.e. when
    DisplayVerifiedBootMenu() succeeds. And the waits are already being
    handled in VerifiedBootMenuShowScreen().
    
    Leave the Red timeout be, though.
    
    Change-Id: I6d2e9e87928fecf5bdfa5fe269726d5139f83369

--- QcomModulePkg/Library/avb/VerifiedBoot.c
+++ QcomModulePkg/Library/avb/VerifiedBoot.c
@@ -1509,7 +1509,6 @@ DisplayVerifiedBootScreen (BootInfo *Info)
     } else {
       DEBUG ((EFI_D_INFO, "Your device has loaded a different operating system."
                           "\nWait for 5 seconds before proceeding\n"));
-      MicroSecondDelay (5000000);
     }
     break;
   case ORANGE:
@@ -1522,7 +1521,6 @@ DisplayVerifiedBootScreen (BootInfo *Info)
       } else {
         DEBUG (
             (EFI_D_INFO, "Device is unlocked, Skipping boot verification\n"));
-        MicroSecondDelay (5000000);
       }
     }
     break;
@@ -1540,7 +1538,6 @@ DisplayVerifiedBootScreen (BootInfo *Info)
       } else {
         DEBUG ((EFI_D_INFO, "The dm-verity is not started in restart mode." \
               "\nWait for 30 seconds before proceeding\n"));
-        MicroSecondDelay (30000000);
       }
   }
 
